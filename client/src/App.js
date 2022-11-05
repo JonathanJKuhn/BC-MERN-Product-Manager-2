66339aaef68e5fb4927f601cc72c7a0677779840
Jonathan Kuhn <jkuhn96@gmail.com>
2022-11-05 03:51:14 -0400

    Initial Commit w/ base code

---
+++ client/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import ProductForm from './components/ProductForm';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+function App() {
+  return (
+    <div className="App">
+      <ProductForm />
+    </div>
+  );
+}
+
+export default App;
